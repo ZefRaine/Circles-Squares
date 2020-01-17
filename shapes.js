8f1bfa4c38c8a56e89309e34ef0d130d9ed2dfc2
zeftambornino@gmail.com <zeftambornino@gmail.com>
2020-01-17 08:54:36 -0600

    init commit w/ basic svg squares

---
+++ shapes.js
@@ -0,0 +1,7 @@
+function mySquare() {
+  document.getElementById("square").style.width = Math.floor(Math.random() * 100) + 1;
+  document.getElementById("square").style.height = Math.floor(Math.random() * 100) + 1;
+};
+function myCircle() {
+  document.getElementById("circle").style.r = Math.floor(Math.random() * 100) + 1;
+};
