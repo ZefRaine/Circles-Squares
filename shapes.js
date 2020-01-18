31a239ebbbea6e8aa1ad9880fe89cc5ca309770c
zeftambornino@gmail.com <zeftambornino@gmail.com>
2020-01-17 23:02:36 -0600

    created all my methods to grab and display my information. Now I just have to organize it

--- shapes.js
+++ shapes.js
@@ -1,23 +1,56 @@
-var i, l;
-var svgns = "http://www.w3.org/2000/svg";
-for (var i = 0; i < 100; i++) {
-  var x = (Math.floor(Math.random() * 100) + 1)
-  y = x;
-
-  var rect = document.createElementNS(svgns, 'rect');
+const svgns = "http://www.w3.org/2000/svg";
+let squareShape, circleShape = [];
+for (let i = 0; i < 50; i++) {
+  let x = (Math.floor(Math.random() * 100) + 1);
+  const rect = document.createElementNS(svgns, 'rect');
   rect.setAttributeNS(null, 'height', x);
-  rect.setAttributeNS(null, 'width', y);
+  rect.setAttributeNS(null, 'width', x);
   rect.setAttributeNS(null, 'stroke-width', '5')
   rect.setAttributeNS(null, 'fill', 'red');
   rect.setAttributeNS(null, 'stroke', 'black');
   document.getElementById('svgSquare').appendChild(rect);
-}
-for (var l = 0; l < 100; l++) {
-  var r = (Math.floor(Math.random() * 100) + 1)
-  var circle = document.createElementNS(svgns, 'circle');
+  let side = rect.height.baseVal.value;
+  let s = side.toString();
+  document.getElementById("dimensions").innerHTML = s;
+
+  function getArea(side, side, unit) {
+    area = side * side;
+    return area + unit;
+  };
+  console.log(`Square: Size = ${side} units, Area = ${getArea(side, side, " units²")}`);
+  document.write(`Square: Size = ${side} units, Area = ${getArea(side, side, " units²")}` + `\n`);
+};
+for (let l = 0; l < 50; l++) {
+  let r = (Math.floor(Math.random() * 100) + 1)
+  const circle = document.createElementNS(svgns, 'circle');
   circle.setAttributeNS(null, 'r', r);
   circle.setAttributeNS(null, 'stroke-width', '5')
   circle.setAttributeNS(null, 'fill', 'blue');
   circle.setAttributeNS(null, 'stroke', 'black');
   document.getElementById('svgCircle').appendChild(circle);
-}
+  let side = circle.r.baseVal.value;
+  let s = side.toString();
+  document.getElementById("dimensions").innerHTML = s;
+
+  function getArea(side, side, unit) {
+    area = side * Math.PI;
+    return area + unit;
+  };
+  console.log(`Circle: Radius = ${side} units, Area = ${getArea(side, side, " units²")}`);
+  document.write(`Circle: Radius = ${side} units, Area = ${getArea(side, side, " units²")}` + `\n`);
+};
+// const bubbleSort = arr => {
+//   let swapped;
+//   do {
+//     swapped = false;
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] > arr[i + 1]) {
+//         let tmp = arr[i];
+//         arr[i] = arr[i + 1];
+//         arr[i + 1] = tmp;
+//         swapped = true;
+//       }
+//     }
+//   } while (swapped);
+//   return arr;
+// };
