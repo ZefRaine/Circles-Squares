8c5af7a5df192450161c1adae9bbf609092184b6
zeftambornino@gmail.com <zeftambornino@gmail.com>
2020-01-24 11:15:13 -0600

    made code less dry

--- shapes.js
+++ shapes.js
@@ -14,14 +14,23 @@ $(document).ready(function () {
 
     function getArea(size, size, unit) {
       area = size * size;
-      xArea = area.toString()
+      xArea = area.toString(); 
       return xArea;
     };
-    let Squares = [{
-      Size: size,
-      Area: getArea(size, size, null)
+    let Shapes = [{
+      Square: {
+        Size: size,
+        Area: getArea(size, size, null)
+      }
+    }, {
+      Circle: {
+        Radius: size,
+        Area: parseFloat(getArea(size, size, null)*Math.PI).toFixed(2)
+      }
     }];
-    console.log(Squares[0]);
+    console.log(Shapes[0]);
+    console.log(Shapes[1]);
+    const circle = document.createElement("div");
     const square = document.createElement("div");
     square.classList.add("mySquare");
     document.body.appendChild(square);
@@ -29,36 +38,10 @@ $(document).ready(function () {
     square.style.height = size + 'px';
     square.style.background = 'red';
     square.style.border = '1px solid black';
-  }
-});
-$(document).ready(function () {
-  var arr = [0];
-
-  for (var i = 1; i <= 50; i++) {
-    var x = (Math.floor(Math.random() * 100) + 1)
-    arr.push(x);
-  }
-  //sort the array descending:
-  arr.sort((a, b) => b - a);
-
-  for (var i = 0; i < 50; i++) {
-    var x = arr[i];
-    var radius = x.toString();
-
-    function getArea(radius, radius, unit) {
-      area = radius * Math.PI;
-      return parseFloat(area).toFixed(2);
-    };
-    let Circles = [{
-      Radius: radius,
-      Area: getArea(radius, radius, null)
-    }];
-    console.log(Circles[0]);
-    const circle = document.createElement("div");
     circle.classList.add("myCircle");
     document.body.appendChild(circle);
-    circle.style.width = radius + 'px';
-    circle.style.height = radius + 'px';
+    circle.style.width = size + 'px';
+    circle.style.height = size + 'px';
     circle.style.background = 'blue';
     circle.style.border = '1px solid black';
   }
